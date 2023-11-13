51a2932566ba0450dd3a14feeb00a32037361667
Mauro Sonzogni <maurosonzo1997@gmail.com>
2023-11-13 16:46:11 +0100

    Update similarity files

--- architectural-cluster.py
+++ architectural-cluster.py
@@ -7,11 +7,10 @@ import matplotlib.pyplot as plt
 from scipy.spatial.distance import  squareform
 
 # File path
-file_path = './report/greedy_structural_symilarity_70.csv'
+file_path = './report/single_level/OR_single.match.structural.similarity.rule_02.csv'
 
 # Load data from the CSV file
 data = pd.read_csv(file_path)
-
 
 
 # Extract the similarity similarities from the dataframe
@@ -23,7 +22,6 @@ print(similarities)
 # Converti la matrice delle distanze in una matrice di distanza condensata
 # questo perchè la nostra è già una matrice di distanza, ma non nella forma che serve
 condensed_distance_matrix = squareform(similarities)
-
 
 Z = hierarchy.linkage(condensed_distance_matrix, 'average')
 
