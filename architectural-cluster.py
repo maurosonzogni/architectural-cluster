a5c0cd279e0c7285eecc0115e7156baf8ccbdc49
Mauro Sonzogni <maurosonzo1997@gmail.com>
2023-12-05 18:03:37 +0100

    Add cluster topic
    - Add cluster topic computation
    - Write topic, number and labels of all cluster inside a .xlsx file

--- architectural-cluster.py
+++ architectural-cluster.py
@@ -7,12 +7,77 @@ import matplotlib.pyplot as plt
 from scipy.spatial.distance import  squareform
 from sklearn.metrics import silhouette_score
 
+# per inferire il topic
+from nltk import FreqDist
+from nltk.tokenize import  word_tokenize
+
+import re
+
+def remove_numbers(sentence):
+    # Utilizza un'espressione regolare per eliminare i numeri e l'underscore attaccato
+    sentence_without_numbers = re.sub(r'\d+_', '', sentence)
+    return sentence_without_numbers
+
+def infer_cluster_label(cluster_labels):
+    
+    # Unire tutte le etichette del cluster in un unico testo
+    cluster_text = ' '.join(cluster_labels)
+
+    cluster_text = remove_numbers(cluster_text)
+    # Replace underscores with spaces
+    cluster_text = cluster_text.replace("_", " ")
+    
+    
+    
+    cluster_text = cluster_text.lower()
+
+    # remove circumstances common words
+    cluster_text = cluster_text.replace("this", "")
+    # Se si rimuove instance alcuni modelli è come se non avessero parole, es 173 e 852
+    cluster_text = cluster_text.replace("instance", "")
+    cluster_text = cluster_text.replace("impl", "")
+    cluster_text = cluster_text.replace("imp", "")
+    cluster_text = cluster_text.replace("sensor", "")
+    cluster_text = cluster_text.replace("system", "")
+    cluster_text = cluster_text.replace("subsystem", "")
+    # single e dual?
+    cluster_text = cluster_text.replace("single", "")
+    cluster_text = cluster_text.replace("dual", "")
+    # integration e standard
+    cluster_text = cluster_text.replace("integration", "")
+    cluster_text = cluster_text.replace("standard", "")
+    # with hardware sub devices
+    cluster_text = cluster_text.replace("with", "")
+    cluster_text = cluster_text.replace("sub", "")
+    cluster_text = cluster_text.replace("functional", "")
+    cluster_text = cluster_text.replace("devices", "")
+    cluster_text = cluster_text.replace("hardware", "")
+
+
+    #
+    cluster_text = cluster_text.replace("\n", "")
+    
+
+    # Tokenizzazione delle parole
+    words = word_tokenize(cluster_text)
+
+    # Calcolo della frequenza delle parole
+    freq_dist = FreqDist(words)
+
+    # Estrazione delle parole chiave (ad esempio, le prime 3 parole più comuni)
+    keywords = freq_dist.most_common(2)
+
+    # Creazione di un'etichetta rappresentativa utilizzando le parole chiave
+    cluster_label = ' '.join(words for words, freq_dist in keywords)
+
+    return cluster_label
+
 # File path
-file_path= './report/multiple_level/OR_single.match.structural.similarity.rule_08.csv'
+file_path= './report/multiple_level/OR_single.match.structural.similarity.rule_06.csv'
+#file_path= 'similarity_values.csv'
 
 # Load data from the CSV file
 data = pd.read_csv(file_path)
-
 
 # Extract the similarity similarities from the dataframe
 similarities = data.iloc[1:, :].values  # Exclude the "model_name" column
@@ -74,8 +139,30 @@ for i, label in enumerate(labels):
         clusters[label].append(data['model_name'].values[i])
 
 # Stampa le etichette per ciascun cluster
+#for cluster_num, cluster_labels in clusters.items():
+   # cluster_label = infer_cluster_label(cluster_labels)
+    #print(f"Cluster {cluster_label} ({cluster_num}): {cluster_labels}")
+# Creazione di un DataFrame
+df_data = []
+
 for cluster_num, cluster_labels in clusters.items():
-    print(f"Cluster {cluster_num}: {cluster_labels}")
+    cluster_label = infer_cluster_label(cluster_labels)
+    df_data.append([cluster_label, cluster_num, cluster_labels])
+
+# Definizione delle colonne
+columns = ['cluster_topic', 'cluster_number', 'contained_models_name']
+
+# Creazione del DataFrame
+df = pd.DataFrame(df_data, columns=columns)
+
+# Salvataggio del DataFrame in un file Excel
+excel_file_path = 'output_clusters.xlsx'
+df.to_excel(excel_file_path, index=False)
+
+# Stampa del DataFrame
+print(df)
+
+print(f"\nI risultati sono stati salvati nel file Excel: {excel_file_path}")
 
 # Calcola l'indice di silhouette per tutti i campioni insieme
 # Nota: se si vuole calcolare la silhouette dei singoli cluster è necessario che il cluster contenga almeno 2 label
@@ -84,4 +171,12 @@ print(f"Silhouette Score dell'intero clustering: {silhouette_avg}")
 
     
 plt.show()
+
+
+
+
+
+
+
+
 
